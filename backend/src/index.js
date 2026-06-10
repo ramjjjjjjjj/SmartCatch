dedaa9733f51fc153a30c22a031f6835e5650e18
ramjjjjjjjj <ramazanff829@gmail.com>
2026-06-10 13:39:19 +0500

    use railway PORT env

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,7 @@ const catchesRouter = require('./routes/catches');
 const quotasRouter = require('./routes/quotas');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
+const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
